0b920a340258667743f78faf9ba43f9dc71a17c9
Danilo Soares <fdanilosoares@gmail.com>
2024-10-25 20:02:20 -0300

    feat: incluir tipo request/response user

---
+++ src/controller/createUser.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/FDaniloSoares/awesomeProject/src/configuration/rest_err"
+	"github.com/FDaniloSoares/awesomeProject/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
---
+++ src/controller/findUser.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindUserById(c *gin.Context) {
+	// some code
+}
+
+func FindUserByEmail(c *gin.Context) {
+	// some code
+}
